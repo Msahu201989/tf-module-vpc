74cb76c56529154f103251f7b0bc2bba1a9f8f82
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-18 21:58:26 +0530

    Terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -7,9 +7,10 @@ resource "aws_subnet" "main" {
     Name = each.key
   }
 }
+
 resource "aws_route_table" "main" {
   for_each = var.subnets
-  vpc_id = var.vpc_id
+  vpc_id   = var.vpc_id
   tags = {
     Name = each.key
   }
